09a19bae430c59ca91efa4a76ecce31ecc71a808
Harrand <harrysjh98@gmail.com>
2022-10-08 00:32:18 +0100

    * Implemented help command

--- commands.lua
+++ commands.lua
@@ -1,29 +1,52 @@
 local addon_name, chatter = ...
+
+chatter.Command = {description = "", subcmd = "", aliases = {}, func = nil}
+function chatter.Command:help()
+	print(chatter.util.colour("00FFFF") .. "/chatter " .. self.subcmd .. chatter.util.colour() .. " - " .. self.description)
+end
+
+-- Write all supported commands
+chatter.Commands = {}
+
+-- Firstly is the 'help' meta-command
+chatter.print_help = function()
+	print("==== " .. chatter.util.colour("FFFF00") .. "Chatterbox" .. chatter.util.colour() .. " Help" .. " ====")
+	for name, command in pairs(chatter.Commands) do
+		command:help()
+	end
+	print("======================")
+end
+
+chatter.Commands["help"] = chatter.Command
+
+-- /chatter help
+chatter.Commands["help"].description = "Displays help about all commands, or pass in a command name to view information about a specific command"
+chatter.Commands["help"].subcmd = "help"
+chatter.Commands["help"].func = function(args)
+	if args == nil or #args == 0 then
+		chatter.print_help()
+	else
+		local subcmd = args[1]
+		chatter.Commands[subcmd]:help()
+	end
+end
+
+-- Implementation specific, WoW command hookup.
+
 SLASH_CHATTER1 = "/chatter"
 SlashCmdList.CHATTER = function(msg, _)
 	local args = {strsplit(" ", msg)}
 
 	local cmd = args[1]
 	if(#cmd == 0) then
-		print("Chatterbox by Harrand")
+		chatter.print_help()
 	else
-		if cmd == "quotes" then
-
-		elseif cmd == "new" then
-
-		elseif cmd == "finish" then
-
-		elseif cmd == "clear" then
-
-		elseif cmd == "quote" then
-
-		elseif cmd == "line" then
-
-		elseif cmd == "test" then
-			print("Marco!")
-			chatter.util.delay(1000.0, function() print("...") chatter.util.delay(4000.0, function() print("Polo!") end) end)
-		else
-			print("Unknown subcmd " .. args[1])
+		for name, command in pairs(chatter.Commands) do
+			if cmd == command.subcmd then
+				command.func({select(2, unpack(args))})
+				return
+			end
 		end
+		print("Unknown subcmd " .. cmd .. ". Type " .. chatter.util.colour("00FFFF") .. "/chatter help" .. chatter.util.colour() .. " for help")
 	end
 end
--- util.lua
+++ util.lua
@@ -1,29 +1,42 @@
 local addon_name, chatter = ...
+-- API Setup
+chatter.util = {}
+chatter.util.impl = {}
 
+-- COLOUR FUNCTIONALITY
+chatter.util.colour = function(colourcode)
+	if colourcode == nil then
+		return "|r"
+	end
+	-- colourcode is gonna be RRGGBB
+	-- color escape codes are AARRGGBB
+	local argb = "00" .. colourcode
+	return "|c" .. argb
+end
+
+-- DELAY FUNCTIONALITY
 -- Frame used for delay function specifically.
-chatter.delay_frame = CreateFrame("Frame")
-chatter.delay_frame:SetScript("OnUpdate", chatter.delay_function)
+chatter.util.impl.delay_frame = CreateFrame("Frame")
 -- Time when the delay function should execute.
-chatter.delay_time = 0
+chatter.util.impl.delay_time = 0
 -- 1 if we're waiting on a delay, otherwise 0
-chatter.delay_latch = 0
+chatter.util.impl.delay_latch = 0
 -- Function to execute when delay completes
-chatter.delay_event = nil
+chatter.util.impl.delay_event = nil
 -- This should never change. Contains implementation for checking if time has passed.
-chatter.delay_function = function()
-	if time() >= chatter.delay_time and chatter.delay_latch == 1 then
-		chatter.delay_latch = 0
-		if chatter.delay_event ~= nil then
-			chatter.delay_event()
+chatter.util.impl.delay_function = function()
+	if time() >= chatter.util.impl.delay_time and chatter.util.impl.delay_latch == 1 then
+		chatter.util.impl.delay_latch = 0
+		if chatter.util.impl.delay_event ~= nil then
+			chatter.util.impl.delay_event()
 		end
 	end
 end
-chatter.delay_frame:SetScript("OnUpdate", chatter.delay_function)
+chatter.util.impl.delay_frame:SetScript("OnUpdate", chatter.util.impl.delay_function)
 
 -- Publically-facing API function
-chatter.util = {}
 chatter.util.delay = function(millis, func)
-	chatter.delay_event = func
-	chatter.delay_time = time() + (millis / 1000.0)
-	chatter.delay_latch = 1
+	chatter.util.impl.delay_event = func
+	chatter.util.impl.delay_time = time() + (millis / 1000.0)
+	chatter.util.impl.delay_latch = 1
 end
