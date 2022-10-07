89457cc434428bd2d5f1078e7828c93298544222
Harrand <harrysjh98@gmail.com>
2022-10-07 18:26:50 +0100

    + Initial commit

---
+++ chatter.lua
@@ -0,0 +1,5 @@
+ChatterDB = {}
+
+local ver, _, _, tocver = GetBuildInfo()
+ChatterDB.version = ver
+ChatterDB.tocver = tocver
---
+++ commands.lua
@@ -0,0 +1,4 @@
+SLASH_CHATTER1 = "/chatter"
+SlashCmdList.CHATTER = function(msg, _)
+	local args = strsplit(" ", msg)
+end
